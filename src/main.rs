651db420b8b24470ecd53222c91d2a3501eee782
Gabi Motroc <motrocgabi@gmail.com>
2024-12-01 20:41:26 +0200

    Day 1

---
+++ src/main.rs
@@ -0,0 +1,72 @@
+use std::collections::HashMap;
+use std::io::stdin;
+
+fn main() {
+    part2();
+}
+
+fn part1() {
+    let mut left_col = vec![];
+    let mut right_col = vec![];
+
+    stdin().lines()
+        .for_each(
+            |x| {
+                let a: Vec<i32> = x
+                    .unwrap()
+                    .split_whitespace()
+                    .map(|y| y.parse::<i32>().unwrap())
+                    .collect();
+
+                left_col.push(a[0]);
+                right_col.push(a[1]);
+            }
+        );
+
+    left_col.sort();
+    right_col.sort();
+
+    let mut result = 0;
+    for x in 0..right_col.len()
+    {
+        result += (left_col[x] - right_col[x]).abs();
+    }
+
+    println!("{result:?}");
+}
+
+fn part2() {
+    let mut left_col = vec![];
+    let mut right_col = vec![];
+    stdin().lines()
+        .for_each(
+            |x| {
+                let a: Vec<i32> = x
+                    .unwrap()
+                    .split_whitespace()
+                    .map(|y| y.parse::<i32>().unwrap())
+                    .collect();
+
+                left_col.push(a[0]);
+                right_col.push(a[1]);
+            }
+        );
+
+    let mut hm = HashMap::new();
+    for x in right_col {
+        hm.entry(x).and_modify(|c| *c += 1).or_insert(1);
+    }
+
+    let mut result = 0;
+
+    for x in left_col {
+        match hm.get(&x) {
+            None => {}
+            Some(value) => {
+                result += x * value;
+            }
+        }
+    }
+
+    println!("{result:?}");
+}
