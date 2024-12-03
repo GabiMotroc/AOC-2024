8d7bce17c71098dfa42f34b91b5b209c0007f4e6
Gabi Motroc <motrocgabi@gmail.com>
2024-12-04 01:01:02 +0200

    day 3 part 1

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -1,6 +1,22 @@
 use std::io::stdin;
+use regex::Regex;
 
 pub fn part1() {
+    let mut result: i32 = 0;
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    stdin().lines()
+        .for_each(|line|
+            {
+                for (_, [first, second]) in re.captures_iter(&line.unwrap()).map(|caps| caps.extract()) {
+                    let a: i32 = first.parse().unwrap();
+                    let b: i32 = second.parse().unwrap();
+
+                    result += a * b;
+                }
+            }
+        );
+
+    println!("{result:?}");
 }
-pub fn part2() {
-}
+
+pub fn part2() {}
